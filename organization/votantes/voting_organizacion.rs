dd42f9efa97214e453b468d6eba84a557cc7c121
polblancoo <73712544+polblancoo@users.noreply.github.com>
2023-09-14 10:38:56 -0300

    Add files via upload

---
+++ organization/votantes/voting_organizacion.rs
@@ -0,0 +1,35 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+use ink::{ env::{DefaultEnvironment, Environment},
+         primitives::AccountId,
+};
+
+ //********************** */
+ #[derive(PartialEq, Debug, scale::Encode, scale::Decode)]
+ #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+ pub enum ContractError {
+      
+     YouAreNotVoter,
+     AccounNotListed,
+     NoAdmin,
+       
+ }
+ 
+#[ink::trait_definition]
+pub trait VotingOrganization {
+    
+    #[ink(message)]
+    fn vote_trait(&mut self , caller: AccountId , voter: AccountId)  -> bool;
+   
+    #[ink(message)]
+    fn get_votes_trait(&self, voter: AccountId) -> i32;
+
+    #[ink(message)]
+     fn add_voter_trait(&mut self, caller: AccountId, voter: AccountId) -> bool ;
+
+     #[ink(message)]
+     fn remove_voter_trait(&mut self, caller: AccountId , voter: AccountId) ->bool;
+    // #[ink(message)]
+    // fn change_admin_trait(&mut self, caller: AccountId ,voter: AccountId) ->bool;
+}
+
+ 
