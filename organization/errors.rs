6de5c42682f53f21e6edf69bc1ff42b3c7d4c8da
polblancoo <73712544+polblancoo@users.noreply.github.com>
2023-09-15 12:42:03 -0300

    Add files via upload

---
+++ organization/errors.rs
@@ -0,0 +1,12 @@
+use scale::{Decode, Encode};
+
+/// Possible erroneous results
+#[derive(Debug, PartialEq, Eq, Clone, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum Error {
+    NotAdmin,
+    VoterNotExist,
+    CannotVoteItself,
+    YouAreNotVoter,
+    NftNotSent, 
+}
