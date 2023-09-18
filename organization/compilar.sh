4162e3ee4a3b1241671e569d269ab2dc1dc043a6
polblancoo <73712544+polblancoo@users.noreply.github.com>
2023-09-17 21:34:49 -0300

    Update compilar.sh

--- organization/compilar.sh
+++ organization/compilar.sh
@@ -11,10 +11,10 @@ read accion;
 
 case $accion in
   1)  cargo +stable contract build --manifest-path votantes/Cargo.toml;;
-  2)  cargo +stable contract build --manifest-path psp34_lop/Cargo.toml;;
+  2)  cargo +stable contract build --manifest-path psp34_bis/Cargo.toml;;
   3)  cargo +stable contract build --manifest-path master/Cargo.toml;;
   4)  cargo +stable contract build --manifest-path master/Cargo.toml
-	  cargo +stable contract build --manifest-path psp34_lop/Cargo.toml
+	  cargo +stable contract build --manifest-path psp34_bis/Cargo.toml
 	  cargo +stable contract build --manifest-path votantes/Cargo.toml;;
  
   *) echo "No es una opcion.";; 
